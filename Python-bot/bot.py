612f53caef4a07a38224141425a2bfaff143543e
Marci Venekei <marci.venekei@gmail.com>
2022-02-12 19:36:32 +0100

    Alap angol valami

--- Python-bot/bot.py
+++ Python-bot/bot.py
@@ -1,7 +1,13 @@
 import time as t
 
 def bot():
-    pass
-
+    print("Starting bot ...")
+    for i in reversed(range(3)):
+        t.sleep(1)
+        print(f"{i+1}")
+    print("Say hi!")
+    you = input()
+    if you.lower() in ["hi", "greetings", "good day", "good afternoon", "good evening", "good morning"]:
+        print(f"Bot: You are speaking English? Then {you}")
 if __name__ == "__main__":
     bot()
